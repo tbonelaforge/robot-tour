c15f39f9db139e823525f403adef035e492f948d
Terry Ford <tford@webassign.net>
2013-02-17 20:02:08 -0500

    Updating broken test.

--- binarynodetester.java
+++ binarynodetester.java
@@ -1,50 +1,33 @@
 public class binarynodetester {
     public static void main( String args[] ) {
         BinaryNode node0 = new BinaryNode();
-//        BinaryNode node1 = new BinaryNode();
-//        node0.setChild(0, node1);
-        node0.insert( 2.9 );
+
+        node0.insert( 2.9, 1 );
         
         System.out.println( "After performing 1 insert, the tree looks like:" );
         System.out.println( node0.printTreeAsHTML() );
         
 
-        node0.insert( 10.2 );
+        node0.insert( 10.2, 2 );
         
         System.out.println( "After performing 2 inserts, the tree looks like:" );
         System.out.println( node0.printTreeAsHTML() );
        
 
-        node0.insert( 22.1 );
+        node0.insert( 22.1, 3 );
         
         System.out.println( "After performing 3 inserts, the tree looks like:");
         System.out.println( node0.printTreeAsHTML() );
 
-        node0.insert( 22.125 );
+        node0.insert( 22.125, 4 );
         
         System.out.println( "After performing 4 inserts, the tree looks like:");
         System.out.println( node0.printTreeAsHTML() );
        
 
-        node0.insert( 111.25 );
+        node0.insert( 111.25, 5 );
         System.out.println( "After performing 5 inserts, the tree looks like:");
         System.out.println( node0.printTreeAsHTML() );
-        BinaryNode node1 = node0.children[0];
-        /*
-        //        node1.height = 3;
-        //        node1.children[1].height = 1;
-        //        node1.children[0].height = 2;
-        //        node1.children[0].children[1].height = 1;
-        //        node1.children[0].children[0].height = 1;
-
-        System.out.println("After setting the height the tree looks like:");
-        System.out.println( node0.printTreeAsHTML() );
-   
-
-        node1.rotate( 1 );
-        System.out.println( "After rotating right, the tree looks like:" );
-        System.out.println( node0.printTreeAsHTML() );
-        */
         
         // Insert a number of random double values.
         BinaryNode node50 = new BinaryNode();
@@ -54,12 +37,9 @@ public class binarynodetester {
             6.035, 3.168, 7.961, 6.312, 2.712
         };
         for ( i = 0; i <  10; i++ ) {
-            //            double randomValue = Math.random() * 32;
             double randomValue = badSequence[i];
-            node50.insert( randomValue );
+            node50.insert( randomValue, i + 1 );
             System.out.printf( "Just inserted: %f\n", randomValue );
-            //                        System.out.println( "After inserting the " + i + " random value, the tree looks like:" );
-            //                        System.out.println( node50.printTreeAsHTML() );
         }
         System.out.println( "At the end of all the " + i + " inserts, the tree looks like:");
         System.out.println( node50.printTreeAsHTML() );
